a56ad2f69912b46e75d27d36063c004c1f7181ff
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-05 18:04:27 +0430

    Add association and method in app/models/comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :update_comments_counter
+
+  def author=(user)
+    self.user_id = user.id
+  end
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
