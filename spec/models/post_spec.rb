f40822234b93f614689807441907be065acdb8e9
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-06 22:33:43 +0430

    Add  spec/ folder for the test files

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,90 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  let(:user) do
+    User.create(name: 'John Doe')
+  end
+
+  let(:myPost) do
+    Post.new(
+      title: 'Hello',
+      text: 'This is my first post',
+      comments_counter: 3,
+      likes_counter: 1,
+      author: user
+    )
+  end
+
+  describe 'Validations' do
+    it 'Should return not valid for a blank title' do
+      myPost.title = nil
+      expect(myPost).to_not be_valid
+    end
+
+    it 'Should return not valid for a title more than 250 chars' do
+      myPost.title = 'Lorem blalllllllllllllllllllllllllllll ipsum dolor sit amet, ' \
+                     'consectetur adipiscing elit. Sed auctor vestibulum velit, ' \
+                     'ut eleifend ante mattis in. Maecenas hendrerit, urna nec ' \
+                     'laoreet bibendum, velit dolor ultricies lorem, quis fermentum ' \
+                     'odio massa nec dui.'
+
+      expect(myPost).to_not be_valid
+    end
+
+    it 'Should say not valid for non integer value of comment_counter' do
+      myPost.comments_counter = 'Something'
+      expect(myPost).to_not be_valid
+    end
+
+    it 'Should say valid for an integer value of comment_counter' do
+      myPost.comments_counter = 0
+      expect(myPost).to be_valid
+    end
+
+    it 'Should say not valid for a value less than 0 for comment_counter' do
+      myPost.comments_counter = -3
+      expect(myPost).to_not be_valid
+    end
+
+    it 'Should say not valid for non integer value of like_counter' do
+      myPost.likes_counter = 'Something'
+      expect(myPost).to_not be_valid
+    end
+
+    it 'Should say valid for an integer value of like_counter' do
+      myPost.likes_counter = 0
+      expect(myPost).to be_valid
+    end
+
+    it 'Should say not valid for a value less than 0 for like_counter' do
+      myPost.likes_counter = -3
+      expect(myPost).to_not be_valid
+    end
+  end
+
+  describe 'Methods of post.rb' do
+    it 'Should update post_counter when a new post is created' do
+      user = User.create(name: 'Sana')
+      post = Post.create(title: 'Hello World', author: user)
+
+      post.update_posts_counter
+
+      expect(user.post_counter).to eq(1)
+    end
+
+    it 'Should show the 5 recent comments of a post' do
+      first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Mexico.')
+      first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+      comment1 = Comment.create(post: first_post, user: first_user, text: 'Hi There!', created_at: 6.hours.ago)
+      comment2 = Comment.create(post: first_post, user: first_user, text: 'Hello There!', created_at: 5.hours.ago)
+      comment3 = Comment.create(post: first_post, user: first_user, text: 'This is Sana!', created_at: 4.hours.ago)
+      comment4 = Comment.create(post: first_post, user: first_user, text: 'Coding is challanging!',
+                                created_at: 3.hours.ago)
+      comment5 = Comment.create(post: first_post, user: first_user, text: 'But practice!', created_at: 2.hours.ago)
+      comment6 = Comment.create(post: first_post, user: first_user, text: 'Bye!', created_at: 1.hours.ago)
+      expect(first_post.five_recent_comments).to eq([comment6, comment5, comment4, comment3, comment2])
+      expect(first_post.five_recent_comments).to_not include(comment1)
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                  post_counter: 3)
+
+  describe 'Validations' do
+    it 'Should return not valid for a blank name' do
+      user.name = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'Should return valid for an existing name' do
+      user.name = 'Tom'
+      expect(user).to be_valid
+    end
+
+    it 'Should not allow a non integer post_counter' do
+      user.post_counter = 'Something'
+      expect(user).to_not be_valid
+    end
+
+    it 'Should allow an integer post_counter' do
+      user.post_counter = 7
+      expect(user).to be_valid
+      user.post_counter = 0
+      expect(user).to be_valid
+    end
+
+    it 'Should not allow an integer lower than 0' do
+      user.post_counter = -1
+      expect(user).to_not be_valid
+    end
+  end
+
+  describe 'User Method #recent_posts' do
+    it 'return the recent three posts for a user' do
+      first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Mexico.')
+
+      first_post = Post.create(author: first_user, title: 'First', text: 'First Post', created_at: 4.day.ago)
+      second_post = Post.create(author: first_user, title: 'Second', text: 'Second Post', created_at: 3.day.ago)
+      third_post = Post.create(author: first_user, title: 'Third', text: 'Third Post', created_at: 2.day.ago)
+      fourth_post = Post.create(author: first_user, title: 'Fourth', text: 'Fourth Post', created_at: 1.day.ago)
+
+      recent_posts = first_user.recent_posts
+
+      expect(recent_posts).to eq([fourth_post, third_post, second_post])
+      expect(recent_posts).to_not include(first_post)
+    end
+  end
+end
