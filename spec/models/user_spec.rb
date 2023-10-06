ff3c1c13434c5bec6a52585d1d9dd5c4831afa04
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-07 00:33:24 +0430

    Update models folder

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, type: :model do
   user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
                   post_counter: 3)
 
-  describe 'Validations' do
-    context 'when checking name' do
+  describe 'User model' do
+    context 'Validations when checking name' do
       it 'Should return not valid for a blank name' do
         user.name = nil
         expect(user).to_not be_valid
@@ -17,7 +17,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context 'when checking post_counter' do
+    context 'Validations when checking post_counter' do
       it 'Should not allow a non integer post_counter' do
         user.post_counter = 'Something'
         expect(user).to_not be_valid
@@ -35,22 +35,22 @@ RSpec.describe User, type: :model do
         expect(user).to_not be_valid
       end
     end
-  end
 
-  describe 'User Method #recent_posts' do
-    it 'return the recent three posts for a user' do
-      first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                               bio: 'Teacher from Mexico.')
+    context 'User Method: #recent_posts' do
+      it 'return the recent three posts for a user' do
+        first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                 bio: 'Teacher from Mexico.')
 
-      first_post = Post.create(author: first_user, title: 'First', text: 'First Post', created_at: 4.day.ago)
-      second_post = Post.create(author: first_user, title: 'Second', text: 'Second Post', created_at: 3.day.ago)
-      third_post = Post.create(author: first_user, title: 'Third', text: 'Third Post', created_at: 2.day.ago)
-      fourth_post = Post.create(author: first_user, title: 'Fourth', text: 'Fourth Post', created_at: 1.day.ago)
+        first_post = Post.create(author: first_user, title: 'First', text: 'First Post', created_at: 4.day.ago)
+        second_post = Post.create(author: first_user, title: 'Second', text: 'Second Post', created_at: 3.day.ago)
+        third_post = Post.create(author: first_user, title: 'Third', text: 'Third Post', created_at: 2.day.ago)
+        fourth_post = Post.create(author: first_user, title: 'Fourth', text: 'Fourth Post', created_at: 1.day.ago)
 
-      recent_posts = first_user.recent_posts
+        recent_posts = first_user.recent_posts
 
-      expect(recent_posts).to eq([fourth_post, third_post, second_post])
-      expect(recent_posts).to_not include(first_post)
+        expect(recent_posts).to eq([fourth_post, third_post, second_post])
+        expect(recent_posts).to_not include(first_post)
+      end
     end
   end
 end
