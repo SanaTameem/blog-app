bde10834df7d907321b5ffdf94d8198391a2a0d6
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-14 16:53:50 +0430

    Update app/controllers/application_controller.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    user_id = params[:user_id]
-    User.find_by(id: user_id)
+    User.first
   end
 end
