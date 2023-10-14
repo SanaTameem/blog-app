7f06849e0819e165000e159d8a91dce2c6007c64
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-14 14:23:07 +0430

    Update  config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[new create]
+    end
   end
   root 'users#index'
 end
