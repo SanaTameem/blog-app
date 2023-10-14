e3a51f9a5b4ff4f851ccc0aaeef7f2be5e9e3443
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-14 14:22:35 +0430

    Update app/controllers/posts_controller.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,22 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = Post.new(params.require(:post).permit(:title,:text))
+    @post.author_id = current_user.id
+
+    if @post.save
+      flash[:notice] = "Post was saved succesfully"
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new
+    end
+  end
 end
