1d4f15f0fb890fe5b107ac294efa4795228e437a
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-19 02:35:15 +0430

    Add app/views/devise/ folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
+  devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
